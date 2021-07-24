114dad2784cc3bbe59da43da460b365961ee6fb4
Annie Huang <annie.hn@gmail.com>
2021-07-24 12:48:43 +1000

    Display the post content in [slug].js page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,13 +2,29 @@ import React from 'react';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
+import marked from 'marked';
+import Link from 'next/link';
 
 const PostPage = ({
   frontmatter: { title, date, cover_image },
   slug,
   content,
 }) => {
-  return <div>{title}</div>;
+  return (
+    <>
+      <Link href='/'>
+        <a className='btn btn-back'>Go Back</a>
+      </Link>
+      <div className='card card-page'>
+        <h1 className='post-title'>{title}</h1>
+        <div className='post-date'>Posted on {date}</div>
+        <img src={cover_image} alt='' />
+        <div className='post-body'>
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export async function getStaticPaths() {
