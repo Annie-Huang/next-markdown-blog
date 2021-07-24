ea42dddbe7aba6df758739bde85138357613b244
Annie Huang <annie.hn@gmail.com>
2021-07-24 12:29:03 +1000

    Implement getStaticProps method and getStaticPaths method in [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,7 +1,51 @@
 import React from 'react';
+import fs from 'fs';
+import path from 'path';
 
 const PostPage = () => {
   return <div>post</div>;
 };
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace('.md', ''),
+    },
+  }));
+
+  console.log('paths=', paths);
+  /* print in terminal when refreshing http://localhost:3000/blog/django-crash-course
+    paths= [
+      { params: { slug: 'django-crash-course' } },
+      { params: { slug: 'javascript-performance-tips' } },
+      { params: { slug: 'new-in-php-8' } },
+      { params: { slug: 'python-book-review' } },
+      { params: { slug: 'react-crash-course' } },
+      { params: { slug: 'tailwind-vs-bootstrap' } },
+      { params: { slug: 'writing-great-unit-tests' } }
+    ]
+ */
+
+  return {
+    // paths: [{ params: { slug: 'my slug' } }],
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  console.log('slug=', slug); // return slug= javascript-performance-tips when you refresh on http://localhost:3000/blog/javascript-performance-tips page.
+
+  const markdownWithMeta = fs.readFileSync(
+    path.join('posts', slug + '.md'),
+    'utf-8'
+  );
+
+  return {
+    props: {},
+  };
+}
 
 export default PostPage;
