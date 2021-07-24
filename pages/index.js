5f85ef43a67f769c4cdefd7806a844c097262047
Annie Huang <annie.hn@gmail.com>
2021-07-24 11:36:00 +1000

    Use getStaticProps to retrieve the content for the /posts/test.md file.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
-import Head from 'next/head'
+// The reason you don't get error for fs and path is Next is smart enough to detect that you only use these in getStaticProps,
+// which will sit on the server side, so will not cause problem with client side.
+import fs from 'fs';
+import path from 'path';
+import Head from 'next/head';
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log('posts=', posts);
   return (
     <div>
       <Head>
@@ -8,5 +13,39 @@ export default function Home() {
       </Head>
       <h2>Hello</h2>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  // Get files from the posts dir
+  const files = fs.readdirSync(path.join('posts'));
+
+  // This log will log in terminal (server side), not devtool in webpage (clicent side)
+  console.log('files=', files); // print: [ 'test.md' ]
+
+  // Get slug and frontmatter from posts
+  const posts = files.map((fileName) => {
+    // Create slug
+    const slug = fileName.replace('.md', '');
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', fileName),
+      'utf-8'
+    );
+
+    console.log('markdownWithMeta=', markdownWithMeta); // print out the content of the test.md
+
+    return {
+      slug,
+    };
+  });
+
+  console.log(posts); // print: [ { slug: 'test' } ]
+
+  return {
+    props: {
+      posts: 'The Posts',
+    },
+  };
 }
